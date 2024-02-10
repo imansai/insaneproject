710555beafa9ab12bb05050598036f719e1c0ae0
imansai <im.saibar@gmail.com>
2024-02-10 23:46:17 +0100

    Add all files

---
+++ weirdproject/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      valorax: ["valorax", "serif"],
+      space: ["Space Mono", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
